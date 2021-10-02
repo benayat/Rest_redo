f06b3f19777c69890c82b410495f02263ba588bd
benaya Trabelsi <benaya7@gmail.com>
2021-10-02 15:21:42 +0300

    -set hostandport in application.properties. -added elastic-context, healthcheck+depends-on,and paths update to docker-compose.yml -updated host and node.name, and removed port from elasticsearch.yml config file. -set up ElasticsearchClientConfiguration.java to prevent the "wrong ipv6 host..."

--- src/main/java/com/example/rest_redo/config/elastic/ElasticsearchClientConfiguration.java
+++ src/main/java/com/example/rest_redo/config/elastic/ElasticsearchClientConfiguration.java
@@ -1,12 +1,13 @@
 package com.example.rest_redo.config.elastic;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.http.HttpHost;
-import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
@@ -15,13 +16,17 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @EnableElasticsearchRepositories(basePackages = "com.example.rest_redo.repository")
 @Slf4j
 public class ElasticsearchClientConfiguration  extends AbstractElasticsearchConfiguration {
-    @Value("${management.metrics.export.elastic.host}")
+    @Value("${elastic.hostandport}")
     private String elasticHost;
+    @SneakyThrows
     @Override
     @Bean
     public RestHighLevelClient elasticsearchClient() {
         log.info("the host is:"+elasticHost);
-        return new RestHighLevelClient(RestClient.builder(new HttpHost(elasticHost)));
+        final ClientConfiguration clientConfiguration = ClientConfiguration.create(elasticHost);
+        log.info("just testing "+clientConfiguration);
+        final RestHighLevelClient highLevelClient = RestClients.create(clientConfiguration).rest();
+        return highLevelClient;
     }
 
 }
